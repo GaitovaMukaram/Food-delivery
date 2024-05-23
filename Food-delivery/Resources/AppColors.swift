625f969ecfe5aa09b14385a8f95eefa8a83e9386
GaitovaMukaram <gaitovamukaram@gmail.com>
2024-05-23 15:23:07 +0500

    added new color

---
+++ Food-delivery/Resources/AppColors.swift
@@ -0,0 +1,15 @@
+//
+//  AppColors.swift
+//  Food-delivery
+//
+//  Created by Mukaram Gaitova on 23.05.2024.
+//
+
+import UIKit
+
+enum AppColors {
+    static let accentOrange = UIColor().hex(0xD35400)
+    static let gray = UIColor().hex(0xECF0F1)
+    static let black = UIColor().hex(0x000000)
+    static let background = UIColor().hex(0xE5E5E5)
+}
---
+++ Food-delivery/Resources/Extensions/UIColor+extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor+extension.swift
+//  Food-delivery
+//
+//  Created by Mukaram Gaitova on 23.05.2024.
+//
+
+import UIKit
+
+extension UIColor {
+    func hex(_ rgbValue: UInt64) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: (1.0)
+        )
+    }
+}
